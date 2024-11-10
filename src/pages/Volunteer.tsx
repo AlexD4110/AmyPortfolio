5299dce2271ad21d8d22f3f1a6615f77ed6a2e2b
Alex Deane <alexdeane16@gmail.com>
2024-11-10 14:57:56 -0500

    Got rid of m

--- src/pages/Volunteer.tsx
+++ src/pages/Volunteer.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import VolunteerCarouselComponent from '@/components/volunteer-carousel';
-import { m } from 'framer-motion';
 
 // Carousel slides data
 const slides = [
